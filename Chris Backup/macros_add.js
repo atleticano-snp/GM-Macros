b9337ac36f1e0b05864d9fe2f8f800b6671b400c
atleticano-snp <68756964+atleticano-snp@users.noreply.github.com>
2023-06-01 17:59:01 -0300

    Add files via upload

---
+++ Chris Backup/macros_add.js
@@ -0,0 +1,6 @@
+	import {web} from './macros/spells/web.js';
+	import {circlePower} from './macros/spells/circlePower.js';
+	import {grease} from './macros/spells/grease.js';
+	'web': web,
+	'grease': grease,
+	'circlePower': circlePower,
