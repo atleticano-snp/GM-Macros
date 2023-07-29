17347921d3e37b63ad056fcdb36f5a3c22ac3fcc
atleticano-snp <68756964+atleticano-snp@users.noreply.github.com>
2023-07-29 12:18:30 -0300

    Delete macros_add.js

--- Chris Backup/macros_add.js
+++
@@ -1,6 +0,0 @@
-	import {web} from './macros/spells/web.js';
-	import {circlePower} from './macros/spells/circlePower.js';
-	import {grease} from './macros/spells/grease.js';
-	'web': web,
-	'grease': grease,
-	'circlePower': circlePower,
