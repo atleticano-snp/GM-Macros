5e4be2c193db1356b0e3aa64fde9fdcab25d039c
atleticano-snp <68756964+atleticano-snp@users.noreply.github.com>
2023-07-29 12:19:47 -0300

    Add files via upload

---
+++ Chris Backup/0macros_add.js
@@ -0,0 +1,10 @@
+
+
+import {grease} from './macros/spells/grease.js';
+import {sleetstorm} from './macros/spells/sleetstorm.js';
+import {web} from './macros/spells/web.js';
+
+
+    'grease': grease,
+    'sleetstorm' : sleetstorm,
+    'web': web,
